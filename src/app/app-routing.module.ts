5d23381585f3fd7fa888c8fa6c871f85690b6ae5
piyushkumartrivedi <csepiyush@gmail.com>
2019-03-04 06:56:25 -0500

    commit
    Signed-off-by: piyushkumartrivedi <csepiyush@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Lesson1Component } from './example/lesson1/lesson1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'Lesson', component: Lesson1Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Lesson1Component } from './example/lesson1/lesson1.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Lesson1Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
